f2a810f51c4136271ee293c284f5b3fd71fa0908
Arham Awan <arhamawan125@gmail.com>
2025-09-18 16:16:50 +0000

    light, dark, system these are not working

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -1,46 +1,9 @@
 "use client"
 
 import * as React from "react"
-import { useTheme as useNextTheme } from "next-themes"
-import { create } from "zustand"
-
-type Theme = "dark" | "light" | "system"
-
-type ThemeProviderState = {
-  theme: Theme
-  setTheme: (theme: Theme) => void
-}
-
-const useThemeStore = create<ThemeProviderState>((set) => ({
-  theme: "system",
-  setTheme: (theme) => set({ theme }),
-}))
-
-export function useTheme() {
-  return useThemeStore((state) => ({
-    theme: state.theme,
-    setTheme: state.setTheme,
-  }))
-}
-
-type ThemeProviderProps = {
-  children: React.ReactNode
-  defaultTheme?: Theme
-  storageKey?: string
-} & React.ComponentProps<typeof NextThemesProvider>
-
 import { ThemeProvider as NextThemesProvider } from "next-themes"
+import { type ThemeProviderProps } from "next-themes/dist/types"
 
-export function ThemeProvider({
-  children,
-  ...props
-}: ThemeProviderProps) {
-  const { setTheme: setNextTheme, resolvedTheme } = useNextTheme()
-  const { theme, setTheme } = useTheme()
-
-  React.useEffect(() => {
-    setNextTheme(theme)
-  }, [theme, setNextTheme])
-
+export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>
 }
